4d8826eabf01f700619c739c18be703e6c419e83
Ivan Dzhurov <ivan.dzhurov@spintech.bg>
2017-11-25 23:56:18 +0200

    added source maps with ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'dist/main.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/scripts')
-  }
+entry: './src/main.ts',
+devtool: 'source-map',
+module: {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }
+  ]
+},
+resolve: {
+  extensions: [ '.tsx', '.ts', '.js' ]
+},
+output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'dist')
+}
 };
