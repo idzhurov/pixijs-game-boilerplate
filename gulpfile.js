f275e414da1f3e10d277c77555d1009f2b67eaa4
Ivan Dzhurov <ivan.dzhurov@spintech.bg>
2017-11-25 19:42:50 +0200

    added webpack, typings & gulpfile for compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var webpack = require('webpack-stream');
+var OUT_DIR = './dist';
 
-gulp.task("default", function () {
+// always copy the html first to dist folder
+var srcIndex = './src/templates/index.html';
+gulp.task('copy:html', function() {
+	gulp.src(srcIndex).pipe(gulp.dest(OUT_DIR));
+});
+
+// compile typescript
+gulp.task("ts:build", function () {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
 });
+
+// run webpack to compile the script into a bundle
+gulp.task('webpack', function() {
+	return gulp.src('dist/')
+    .pipe(webpack( require('./webpack.config.js') ))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('other-task', ['ts:build'], function() {
+  return gulp.src('dist/')
+    .pipe(webpack( require('./webpack.config.js') ))
+    .pipe(gulp.dest('dist/'));
+});
+
+// default includes all
+gulp.task('default', ['copy:html', 'other-task']);
